f82cb4c6a37cfb570def671ed7f87c9f866fa220
winvenkat <62222647+winvenkat@users.noreply.github.com>
2020-04-05 20:43:38 -0400

    Add files via upload

---
+++ tasks/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+	title = models.CharField(max_length=200)
+	complete = models.BooleanField(default=False)
+	created = models.DateField(auto_now_add=True)
+	targetdate = models.DateField()	
+	notes = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.title
